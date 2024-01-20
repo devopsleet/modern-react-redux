cc0165efdc500bd402a2618cc94c80b021194ff3
gagan <gasingla07@gmail.com>
2024-01-20 11:34:12 -0600

    more on JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,6 +1,7 @@
 // 1) Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
+import MyApp from './App';
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById("root");
@@ -8,29 +9,6 @@ const el = document.getElementById("root");
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
-// 4) Create a component
-function App() {
 
-  return <input style={{border: '1px solid blue'}}/>
-
-  //const inputType = "number";
-  //const minValue = 5;
-
-  //return <input type={inputType} min = {minValue}/>
-  //return <input style = {{border: '3px solid red'}} type="number" min = {5}/>
-
-  //return <textarea autoFocus={true}/>
-  // const message = [1,2,3];
-  // const date = new Date();
-  // const time = date.toLocaleDateString();
-  // const name = "gagan";
-  // const age = 23;
-  // let message = "Bye there"
-  // if (Math.random () > 0.5) {
-  //     message = 'Hello There'
-  // }
-  // return <h1>Hi, My name is {name} {age}</h1>;
-}
-
-// 5) Show the component on the screen
-root.render(<App />);
+// 4) Show the component on the screen
+root.render(<MyApp />);
