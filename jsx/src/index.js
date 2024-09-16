fe70d87548e939c5042e05b218771f5dac4d27f6
gagan <gasingla07@gmail.com>
2024-09-16 16:00:33 -0500

    new project

---
+++ jsx/src/index.js
@@ -0,0 +1,31 @@
+// 1) Import th React and ReactDOM libraries
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// 2) Get a reference to the div with ID root
+
+const el = document.getElementById("root");
+
+// 3) Tell React to take control of that element
+
+const root = ReactDOM.createRoot(el);
+
+// 4) Create a Component
+
+function App() {
+
+    
+
+    // let message = 'Bye there'
+    // if (Math.random() > 0.5) {
+    //     message = 'Hello there';
+    // }
+    
+const date = new Date();
+const time = date.toLocaleDateString();
+  return <h1>{time}</h1>;
+}
+
+// 5) Show the component on the screen
+root.render(<App />);
